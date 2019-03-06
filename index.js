5bc3b27ac5bc3c9135b9b02c1e9bdcc116b4e66c
50jeffng <50jeffng@gmail.com>
2019-03-07 10:28:46 +1100

    Fixed mobile view, and typos

--- index.js
+++ index.js
@@ -3,11 +3,41 @@ console.log("Hello there ! Fellow developer !\nI have coded this using Bootstrap
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
-        if (document.getElementById('content').style.marginLeft === '80px'){
-            document.getElementById('content').style.marginLeft = '250px';
+        if ($(window).width() <= 768) {
+            if (document.getElementById('content').style.marginLeft != '80px'){
+                document.getElementById('content').style.marginLeft = '80px';
+            }
+            else {
+                document.getElementById('content').style.marginLeft= '0px';
+            }
         }
         else {
-            document.getElementById('content').style.marginLeft= '80px';
+            if (document.getElementById('content').style.marginLeft != '80px'){
+                document.getElementById('content').style.marginLeft = '80px';
+            }
+            else {
+                document.getElementById('content').style.marginLeft= '250px';
+            }
         }
     });
+});
+
+$(window).resize(function() {
+    $('#sidebar').toggleClass('active');
+    if ($(window).width() <= 768) {
+        if (document.getElementById('content').style.marginLeft != '80px'){
+            document.getElementById('content').style.marginLeft = '80px';
+        }
+        else {
+            document.getElementById('content').style.marginLeft= '0px';
+        }
+    }
+    else {
+        if (document.getElementById('content').style.marginLeft != '80px'){
+            document.getElementById('content').style.marginLeft = '80px';
+        }
+        else {
+            document.getElementById('content').style.marginLeft= '250px';
+        }
+    }
 });
